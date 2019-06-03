fa74679b2005a90645a4a09a93627556d4fc75d1
arkai <k79k06k02k@gmail.com>
2019-06-04 02:07:15 +0800

    - Prototype

---
+++ Assets/06 - Prototype/PrototypeInstance.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Prototype
+{
+    public class WorkExp : ICloneable
+    {
+        public string date { get; set; }
+        public string company { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+
+    public class Resume : ICloneable
+    {
+        private string sex;
+        private string age;
+        private WorkExp workExp;
+
+
+        public Resume()
+        {
+            workExp = new WorkExp();
+        }
+
+        public void SetInfo(string sex, string age)
+        {
+            this.sex = sex;
+            this.age = age;
+        }
+
+        public void SetWorkExp(string date, string company)
+        {
+            workExp.date = date;
+            workExp.company = company;
+        }
+
+        public void Display()
+        {
+            Debug.LogFormat("sex:{0}, age:{1}, workExp date:{2}, workExp company:{3}", sex, age, workExp.date, workExp.company);
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public Resume CloneDeep()
+        {
+            Resume resume = (Resume)Clone();
+            resume.workExp = (WorkExp)workExp.Clone();
+            return resume;
+        }
+    }
+}
---
+++ Assets/06 - Prototype/PrototypeTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Prototype
+{
+    public class PrototypeTester : MonoBehaviour
+    {
+        private void Start()
+        {
+            Resume resumeA = new Resume();
+            resumeA.SetInfo("man", "16");
+            resumeA.SetWorkExp("1Y", "FB");
+
+            Resume resumeB = resumeA.CloneDeep();
+            resumeB.SetInfo("gril", "33");
+
+            Resume resumeC = resumeA.CloneDeep();
+            resumeC.SetWorkExp("4Y", "Google");
+
+
+            resumeA.Display();
+            resumeB.Display();
+            resumeC.Display();
+        }
+    }
+}
