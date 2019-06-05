3db6ccae15240db3a1f7d7c148d79e9b1bd5a504
arkai <k79k06k02k@gmail.com>
2019-06-05 23:33:39 +0800

    - Template

---
+++ Assets/07 - Template/TemplateInstance.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Template
+{
+    public class ExamPaper
+    {
+        public void Test1()
+        {
+            Debug.Log("試題一答案: " + Answer1());
+        }
+
+        public void Test2()
+        {
+            Debug.Log("試題二答案: " + Answer2());
+        }
+
+
+        protected virtual string Answer1()
+        {
+            return "";
+        }
+
+        protected virtual string Answer2()
+        {
+            return "";
+        }
+    }
+
+
+    public class StudentPaperA : ExamPaper
+    {
+        protected override string Answer1()
+        {
+            return "A";
+        }
+
+        protected override string Answer2()
+        {
+            return "C";
+        }
+    }
+
+    public class StudentPaperB : ExamPaper
+    {
+        protected override string Answer1()
+        {
+            return "D";
+        }
+
+        protected override string Answer2()
+        {
+            return "A";
+        }
+    }
+}
---
+++ Assets/07 - Template/TemplateTester.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Template
+{
+    public class TemplateTester : MonoBehaviour
+    {
+        private void Start()
+        {
+            StudentPaperA studentPaperA = new StudentPaperA();
+            studentPaperA.Test1();
+            studentPaperA.Test2();
+
+            StudentPaperB studentPaperB = new StudentPaperB();
+            studentPaperB.Test1();
+            studentPaperB.Test2();
+        }
+    }
+}
