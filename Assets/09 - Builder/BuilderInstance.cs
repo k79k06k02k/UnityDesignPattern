4d599898c68cdf460aee3ffa6c33590baeed3a8d
arkai <k79k06k02k@gmail.com>
2019-06-12 23:35:35 +0800

    - Builder

---
+++ Assets/09 - Builder/BuilderInstance.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace DP.Builder
+{
+    public abstract class BuildBase
+    {
+        public abstract void SetupType();
+        public abstract void SetupGroud();
+        public abstract void SetupHeight();
+    }
+
+    public class BuildA : BuildBase
+    {
+        public override void SetupType()
+        {
+            Debug.Log("[BuildA] 類型: 台式三合院");
+        }
+
+        public override void SetupGroud()
+        {
+            Debug.Log("[BuildA] 佔地: 10公頃");
+        }
+
+        public override void SetupHeight()
+        {
+            Debug.Log("[BuildA] 高度: 5樓");
+        }
+    }
+
+    public class BuildB : BuildBase
+    {
+        public override void SetupType()
+        {
+            Debug.Log("[BuildB] 類型: 歐式");
+        }
+
+        public override void SetupGroud()
+        {
+            Debug.Log("[BuildB] 佔地: 100公頃");
+        }
+
+        public override void SetupHeight()
+        {
+            Debug.Log("[BuildB] 高度: 3樓");
+        }
+    }
+
+
+    public class BuildDirect
+    {
+        public void CreateBuild(BuildBase build)
+        {
+            build.SetupType();
+            build.SetupGroud();
+            build.SetupHeight();
+        }
+    }
+}
---
+++ Assets/09 - Builder/BuilderTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Builder
+{
+    public class BuilderTester : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            BuildDirect buildDirect = new BuildDirect();
+
+            BuildBase build = new BuildA();
+            buildDirect.CreateBuild(build);
+
+            build = new BuildB();
+            buildDirect.CreateBuild(build);
+        }
+
+    }
+}
