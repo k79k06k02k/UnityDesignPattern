1770a786a6db1e4b3ebe80caccb60440bf815b36
arkai <k79k06k02k@gmail.com>
2019-06-18 22:53:50 +0800

    - Observer delegate

--- Assets/10 - Observer/ObserverTester.cs
+++ Assets/10 - Observer/ObserverTester.cs
@@ -6,7 +6,19 @@ namespace DP.Observer
 {
     public class ObserverTester : MonoBehaviour
     {
+
         private void Start()
+        {
+            TestObserver();
+
+            Debug.Log("-------------------------------------------------------------------------------------");
+
+            TestDelegate();
+        }
+
+
+
+        private void TestObserver()
         {
             SubjectA subjectA = new SubjectA();
 
@@ -15,6 +27,27 @@ namespace DP.Observer
 
             subjectA.status = "Set Status!";
             subjectA.Notify();
+        }
+
+
+
+        private delegate void EventHandler();
+        private event EventHandler OnEventNotify;
+
+        private void TestDelegate()
+        {
+            OnEventNotify += EventObserverA;
+            OnEventNotify += EventObserverB;
+
+            OnEventNotify.Invoke();
+        }
+        private void EventObserverA()
+        {
+            Debug.Log("EventObserverA Update!");
+        }
+        private void EventObserverB()
+        {
+            Debug.Log("EventObserverB Update!");
         }
     }
 }
