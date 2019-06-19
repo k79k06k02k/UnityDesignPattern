d0faf4f1a3c8849f5062f3332da76034c9ffdee2
arkai <k79k06k02k@gmail.com>
2019-06-19 22:56:51 +0800

    - Abstract Factory

---
+++ Assets/11 - AbstractFactory/AbstractFactoryInstance.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+namespace DP.AbstractFactory
+{
+    public interface IFactory
+    {
+        IUser CreateUser();
+        IDepartment CreateDepartment();
+    }
+    public class SqlServerFactory : IFactory
+    {
+        public IDepartment CreateDepartment()
+        {
+            return new SqlServerDepartment();
+        }
+
+        public IUser CreateUser()
+        {
+            return new SqlServerUser();
+        }
+    }
+    public class AccessFactory : IFactory
+    {
+        public IDepartment CreateDepartment()
+        {
+            return new AccessDepartment();
+        }
+
+        public IUser CreateUser()
+        {
+            return new AccessUser();
+        }
+    }
+
+
+    public interface IUser
+    {
+        void Insert();
+        void Get();
+    }
+    public class SqlServerUser : IUser
+    {
+        public void Get()
+        {
+            Debug.Log("Get SqlServerUser");
+        }
+
+        public void Insert()
+        {
+            Debug.Log("Insert SqlServerUser");
+        }
+    }
+    public class AccessUser : IUser
+    {
+        public void Get()
+        {
+            Debug.Log("Get AccessUser");
+        }
+
+        public void Insert()
+        {
+            Debug.Log("Insert AccessUser");
+        }
+    }
+
+
+    public interface IDepartment
+    {
+        void Insert();
+        void Get();
+    }
+    public class SqlServerDepartment : IDepartment
+    {
+        public void Get()
+        {
+            Debug.Log("Get SqlServerDepartment");
+        }
+
+        public void Insert()
+        {
+            Debug.Log("Insert SqlServerDepartment");
+        }
+    }
+    public class AccessDepartment : IDepartment
+    {
+        public void Get()
+        {
+            Debug.Log("Get AccessDepartment");
+        }
+
+        public void Insert()
+        {
+            Debug.Log("Insert AccessDepartment");
+        }
+    }
+}
---
+++ Assets/11 - AbstractFactory/AbstractFactoryTester.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace DP.AbstractFactory
+{
+    public class AbstractFactoryTester : MonoBehaviour
+    {
+        private void Start()
+        {
+            //IFactory factory = new SqlServerFactory();
+            IFactory factory = new AccessFactory();
+
+            IUser user = factory.CreateUser();
+            user.Insert();
+            user.Get();
+
+            IDepartment department = factory.CreateDepartment();
+            department.Insert();
+            department.Get();
+        }
+    }
+}
