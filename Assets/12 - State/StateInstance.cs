7648ea9dc6459fe8fbabb8e16d74693c2d55c7c1
arkai <k79k06k02k@gmail.com>
2019-06-23 17:45:11 +0800

    - State

---
+++ Assets/12 - State/StateInstance.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.State
+{
+    public class Work
+    {
+        public int Time { get; set; }
+        public bool WorkFinish { get; set; }
+
+        private WorkStateBase m_workStateBase;
+
+        public void SetState(WorkStateBase state)
+        {
+            m_workStateBase = state;
+        }
+
+        public void Handler()
+        {
+            m_workStateBase.Handler(this);
+        }
+    }
+
+
+    public abstract class WorkStateBase
+    {
+        public abstract void Handler(Work work);
+    }
+    public class WorkStateA : WorkStateBase
+    {
+        public override void Handler(Work work)
+        {
+            if (work.Time > 10)
+            {
+                Debug.Log("To WorkStateB");
+                work.SetState(new WorkStateB());
+                work.Handler();
+            }
+            else
+            {
+                Debug.Log("Keep WorkStateA");
+            }
+        }
+    }
+    public class WorkStateB : WorkStateBase
+    {
+        public override void Handler(Work work)
+        {
+            if (work.Time > 15)
+            {
+                Debug.Log("To WorkStateC");
+                work.SetState(new WorkStateC());
+                work.Handler();
+            }
+            else
+            {
+                Debug.Log("Keep WorkStateB");
+            }
+        }
+    }
+    public class WorkStateC : WorkStateBase
+    {
+        public override void Handler(Work work)
+        {
+            if (work.WorkFinish)
+            {
+                Debug.Log("WorkFinish");
+            }
+        }
+    }
+}
---
+++ Assets/12 - State/StateTester.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.State
+{
+    public class StateTester : MonoBehaviour
+    {
+        private void Start()
+        {
+            Work work = new Work();
+            work.Time = 9;
+            work.WorkFinish = false;
+            work.SetState(new WorkStateA());
+
+            work.Time = 10;
+            work.Handler();
+
+            work.Time = 13;
+            work.Handler();
+
+            work.Time = 17;
+            work.Handler();
+
+            work.Time = 20;
+            work.WorkFinish = true;
+            work.Handler();
+        }
+    }
+}
