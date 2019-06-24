54489518fc7e0aabe9d8d0a03f023c5234cdfb1d
arkai <k79k06k02k@gmail.com>
2019-06-24 23:51:53 +0800

    - Adapter

---
+++ Assets/13 - Adapter/AdapterInstance.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+
+public interface IPlayer
+{
+    void Attack();
+    void Defence();
+}
+
+public class PlayerNationalA : IPlayer
+{
+    public void Attack()
+    {
+        Debug.Log("NationalA Attack");
+    }
+
+    public void Defence()
+    {
+        Debug.Log("NationalA Defence");
+    }
+}
+
+public class PlayerNationalB : IPlayer
+{
+    public void Attack()
+    {
+        Debug.Log("NationalB Attack");
+    }
+
+    public void Defence()
+    {
+        Debug.Log("NationalB Defence");
+    }
+}
+
+public class PlayerTranslator : IPlayer
+{
+    private PlayerForeignerA playerForeignerA = new PlayerForeignerA();
+
+    public void Attack()
+    {
+        playerForeignerA.ForeignerAttack();
+    }
+
+    public void Defence()
+    {
+        playerForeignerA.ForeignerDefence();
+    }
+}
+
+public class PlayerForeignerA
+{
+    public void ForeignerAttack()
+    {
+        Debug.Log("Foreigner Attack");
+    }
+
+    public void ForeignerDefence()
+    {
+        Debug.Log("Foreigner Defence");
+    }
+}
---
+++ Assets/13 - Adapter/AdapterTester.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Adapter
+{
+    public class AdapterTester : MonoBehaviour
+    {
+        private void Start()
+        {
+            PlayerNationalA playerNationalA = new PlayerNationalA();
+            playerNationalA.Attack();
+            playerNationalA.Defence();
+
+            PlayerTranslator playerTranslator = new PlayerTranslator();
+            playerTranslator.Attack();
+            playerTranslator.Defence();
+        }
+    }
+}
