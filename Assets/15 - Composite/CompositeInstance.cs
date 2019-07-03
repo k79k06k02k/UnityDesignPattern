2ec44c24f64826074474c39ef4492b7225b4da1c
arkai <k79k06k02k@gmail.com>
2019-07-04 00:47:57 +0800

    - Decorator 換 UML - Composite

---
+++ Assets/15 - Composite/CompositeInstance.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Composite
+{
+    public abstract class Company
+    {
+        protected string m_name;
+
+        public Company(string name)
+        {
+            m_name = name;
+        }
+
+        public abstract void Add(Company company);
+        public abstract void Remove(Company company);
+        public abstract void Display(int depth);
+        public abstract void LineOfDuty();
+    }
+
+    public class FinanceDepartment : Company
+    {
+        public FinanceDepartment(string name) : base(name) { }
+
+        public override void Add(Company company) { }
+
+        public override void Remove(Company company) { }
+
+        public override void Display(int depth)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < depth; i++)
+            {
+                result += "-";
+            }
+
+            Debug.Log(result += m_name);
+        }
+
+        public override void LineOfDuty()
+        {
+            Debug.LogFormat("{0} 公司財務管理", m_name);
+        }
+    }
+    public class HRDepartment : Company
+    {
+        public HRDepartment(string name) : base(name) { }
+
+        public override void Add(Company company) { }
+
+        public override void Remove(Company company) { }
+
+        public override void Display(int depth)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < depth; i++)
+            {
+                result += "-";
+            }
+
+            Debug.Log(result += m_name);
+        }
+
+        public override void LineOfDuty()
+        {
+            Debug.LogFormat("{0} 員工招聘與教育訓練", m_name);
+        }
+    }
+    public class ConcreteCompany : Company
+    {
+        private List<Company> m_companies = new List<Company>();
+
+        public ConcreteCompany(string name) : base(name)
+        {
+        }
+
+        public override void Add(Company company)
+        {
+            m_companies.Add(company);
+        }
+
+        public override void Remove(Company company)
+        {
+            m_companies.Remove(company);
+        }
+
+
+        public override void Display(int depth)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < depth; i++)
+            {
+                result += "-";
+            }
+
+            Debug.Log(result += m_name);
+
+
+            for (int i = 0; i < m_companies.Count; i++)
+            {
+                m_companies[i].Display(depth + 2);
+            }
+        }
+
+        public override void LineOfDuty()
+        {
+            for (int i = 0; i < m_companies.Count; i++)
+            {
+                m_companies[i].LineOfDuty();
+            }
+        }
+    }
+}
---
+++ Assets/15 - Composite/CompositeTester.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Composite
+{
+    public class CompositeTester : MonoBehaviour
+    {
+        private void Start()
+        {
+            ConcreteCompany root = new ConcreteCompany("總公司");
+            root.Add(new FinanceDepartment("總公司財務部門"));
+            root.Add(new HRDepartment("總公司HR部門"));
+
+            ConcreteCompany branchAmerican = new ConcreteCompany("美國分公司");
+            branchAmerican.Add(new FinanceDepartment("美國分公司財務部門"));
+            branchAmerican.Add(new HRDepartment("美國分公司HR部門"));
+            root.Add(branchAmerican);
+
+            ConcreteCompany branchCalifornia = new ConcreteCompany("加州辦事處");
+            branchCalifornia.Add(new FinanceDepartment("加州辦事處財務部門"));
+            branchCalifornia.Add(new HRDepartment("加州辦事處HR部門"));
+            branchAmerican.Add(branchCalifornia);
+
+            ConcreteCompany branchNewYork = new ConcreteCompany("紐約辦事處");
+            branchNewYork.Add(new FinanceDepartment("紐約辦事處財務部門"));
+            branchNewYork.Add(new HRDepartment("紐約辦事處HR部門"));
+            branchAmerican.Add(branchNewYork);
+
+            Debug.Log("組織結構：");
+            root.Display(1);
+
+            Debug.Log("職責：");
+            root.LineOfDuty();
+        }
+    }
+}
