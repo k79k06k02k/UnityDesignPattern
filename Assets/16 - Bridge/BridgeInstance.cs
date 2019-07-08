883c9101bf801fc221aa74c5a5940ac849bf0533
arkai <k79k06k02k@gmail.com>
2019-07-08 23:34:12 +0800

    - Bridge

---
+++ Assets/16 - Bridge/BridgeInstance.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Bridge
+{
+    public abstract class MobileBrandBase
+    {
+        protected MobileSoftwareBase m_mobileSoftwareBase;
+
+        public void SetSoftwave(MobileSoftwareBase soft)
+        {
+            m_mobileSoftwareBase = soft;
+        }
+
+        public abstract void Run();
+    }
+    public class MobileBrandA : MobileBrandBase
+    {
+        public override void Run()
+        {
+            Debug.Log("MobileBrandA Run");
+            m_mobileSoftwareBase.Run();
+        }
+    }
+    public class MobileBrandB : MobileBrandBase
+    {
+        public override void Run()
+        {
+            Debug.Log("MobileBrandB Run");
+            m_mobileSoftwareBase.Run();
+        }
+    }
+
+
+    public abstract class MobileSoftwareBase
+    {
+        public abstract void Run();
+    }
+    public class MobileSoftwareGame : MobileSoftwareBase
+    {
+        public override void Run()
+        {
+            Debug.Log("MobileSoftwareGame Run");
+        }
+    }
+    public class MobileSoftwareAddressList : MobileSoftwareBase
+    {
+        public override void Run()
+        {
+            Debug.Log("MobileSoftwareAddressList Run");
+        }
+    }
+}
---
+++ Assets/16 - Bridge/BridgeTester.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Bridge
+{
+    public class BridgeTester : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            MobileBrandBase mobileBrandBase = new MobileBrandA();
+
+            mobileBrandBase.SetSoftwave(new MobileSoftwareGame());
+            mobileBrandBase.Run();
+
+            mobileBrandBase.SetSoftwave(new MobileSoftwareAddressList());
+            mobileBrandBase.Run();
+
+
+
+            mobileBrandBase = new MobileBrandB();
+
+            mobileBrandBase.SetSoftwave(new MobileSoftwareGame());
+            mobileBrandBase.Run();
+
+            mobileBrandBase.SetSoftwave(new MobileSoftwareAddressList());
+            mobileBrandBase.Run();
+        }
+
+    }
+}
