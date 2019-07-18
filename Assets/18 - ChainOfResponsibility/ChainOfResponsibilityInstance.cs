3a87fac15047fabfdb5e49faaae430ffe02edc72
arkai <k79k06k02k@gmail.com>
2019-07-18 23:04:11 +0800

    - Chain Of Responsibility

---
+++ Assets/18 - ChainOfResponsibility/ChainOfResponsibilityInstance.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.ChainOfResponsibility
+{
+    public abstract class ManagerBase
+    {
+        protected string m_name;
+        protected ManagerBase m_superior;
+
+
+        public ManagerBase(string name)
+        {
+            m_name = name;
+        }
+
+        public void SetSuperior(ManagerBase superior)
+        {
+            m_superior = superior;
+        }
+
+        public abstract void Request(int value);
+    }
+
+    public class ManagerA : ManagerBase
+    {
+        public ManagerA(string name) : base(name)
+        {
+        }
+
+        public override void Request(int value)
+        {
+            if (value < 100)
+            {
+                Debug.Log("Request: " + m_name);
+            }
+            else if (m_superior != null)
+            {
+                Debug.Log("Superior Request: " + m_name);
+                m_superior.Request(value);
+            }
+        }
+    }
+    public class ManagerB : ManagerBase
+    {
+        public ManagerB(string name) : base(name)
+        {
+        }
+
+        public override void Request(int value)
+        {
+            if (value < 20)
+            {
+                Debug.Log("Request: " + m_name);
+            }
+            else if (m_superior != null)
+            {
+                Debug.Log("Superior Request: " + m_name);
+                m_superior.Request(value);
+            }
+        }
+    }
+    public class ManagerC : ManagerBase
+    {
+        public ManagerC(string name) : base(name)
+        {
+        }
+
+        public override void Request(int value)
+        {
+            if (value < 10)
+            {
+                Debug.Log("Request: " + m_name);
+            }
+            else if (m_superior != null)
+            {
+                Debug.Log("Superior Request: " + m_name);
+                m_superior.Request(value);
+            }
+        }
+    }
+}
---
+++ Assets/18 - ChainOfResponsibility/ChainOfResponsibilityTester.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.ChainOfResponsibility
+{
+    public class ChainOfResponsibilityTester : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            ManagerA managerA = new ManagerA("管理者 A 級");
+            ManagerB managerB = new ManagerB("管理者 B 級");
+            ManagerC managerC = new ManagerC("管理者 C 級");
+
+            managerC.SetSuperior(managerB);
+            managerB.SetSuperior(managerA);
+
+            managerC.Request(7);
+            Debug.Log("--------------------------------------");
+
+            managerC.Request(13);
+            Debug.Log("--------------------------------------");
+
+            managerC.Request(22);
+            Debug.Log("--------------------------------------");
+
+            managerC.Request(50);
+            Debug.Log("--------------------------------------");
+        }
+
+    }
+}
