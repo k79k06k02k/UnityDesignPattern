0107e9a9a5be6d1c8c989481a125b6b69a491211
arkai <k79k06k02k@gmail.com>
2019-07-21 01:07:39 +0800

    - Mediator

---
+++ Assets/19 - Mediator/MediatorInstance.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Mediator
+{
+    public abstract class CountryMediatorBase
+    {
+        public abstract void Declare(string message, CountryBase country);
+    }
+    public class CountryMediatorA : CountryMediatorBase
+    {
+        private USA m_countryUSA;
+        public USA countryUSA { set { m_countryUSA = value; } }
+
+        private Taiwan m_countryTaiwan;
+        public Taiwan countryTaiwan { set { m_countryTaiwan = value; } }
+
+        public override void Declare(string message, CountryBase country)
+        {
+            if (country == m_countryUSA)
+            {
+                m_countryUSA.ShowMessage(message);
+            }
+            else
+            {
+                m_countryTaiwan.ShowMessage(message);
+            }
+        }
+    }
+
+
+
+    public abstract class CountryBase
+    {
+        protected CountryMediatorBase m_mediator;
+
+        public CountryBase(CountryMediatorBase mediator)
+        {
+            m_mediator = mediator;
+        }
+    }
+    public class USA : CountryBase
+    {
+        public USA(CountryMediatorBase mediator) : base(mediator)
+        {
+        }
+
+        public void Declare(string message)
+        {
+            m_mediator.Declare(message, this);
+        }
+
+        public void ShowMessage(string message)
+        {
+            Debug.Log("USA: " + message);
+        }
+    }
+    public class Taiwan : CountryBase
+    {
+        public Taiwan(CountryMediatorBase mediator) : base(mediator)
+        {
+        }
+
+        public void Declare(string message)
+        {
+            m_mediator.Declare(message, this);
+        }
+
+        public void ShowMessage(string message)
+        {
+            Debug.Log("Taiwan: " + message);
+        }
+    }
+}
---
+++ Assets/19 - Mediator/MediatorTester.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Mediator
+{
+    public class MediatorTester : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            CountryMediatorA countryMediatorA = new CountryMediatorA();
+
+            USA usa = new USA(countryMediatorA);
+            Taiwan taiwan = new Taiwan(countryMediatorA);
+
+            countryMediatorA.countryUSA = usa;
+            countryMediatorA.countryTaiwan = taiwan;
+
+            usa.Declare("Hello");
+            taiwan.Declare("Hey");
+        }
+
+    }
+}
