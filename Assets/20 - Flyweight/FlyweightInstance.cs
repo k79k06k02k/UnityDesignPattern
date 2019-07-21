35bdc2df1f4fcae0282131aa7b1b830297379ccc
arkai <k79k06k02k@gmail.com>
2019-07-21 22:12:54 +0800

    - Flyweight

---
+++ Assets/20 - Flyweight/FlyweightInstance.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Flyweight
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public User(string name)
+        {
+            Name = name;
+        }
+    }
+
+
+    public abstract class WebsiteBase
+    {
+        protected string m_name;
+
+        public WebsiteBase(string name)
+        {
+            m_name = name;
+        }
+
+        public abstract void Use(User user);
+    }
+    public class WebsiteInstance : WebsiteBase
+    {
+        public WebsiteInstance(string name) : base(name)
+        {
+        }
+
+        public override void Use(User user)
+        {
+            Debug.LogFormat("網站名稱:{0}, 使用者:{1}", m_name, user.Name);
+        }
+    }
+
+
+    public class WebsiteFactory
+    {
+        private Hashtable m_flyweights = new Hashtable();
+
+
+        public WebsiteBase GetWebsite(string key)
+        {
+            if (m_flyweights.ContainsKey(key) == false)
+            {
+                m_flyweights.Add(key, new WebsiteInstance(key));
+            }
+
+            return (WebsiteInstance)m_flyweights[key];
+        }
+
+        public int GetCount()
+        {
+            return m_flyweights.Count;
+        }
+    }
+}
---
+++ Assets/20 - Flyweight/FlyweightTester.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DP.Flyweight
+{
+    public class FlyweightTester : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            WebsiteFactory websiteFactory = new WebsiteFactory();
+
+            WebsiteBase websiteBase = websiteFactory.GetWebsite("Website 1");
+            websiteBase.Use(new User("User AA"));
+
+            websiteBase = websiteFactory.GetWebsite("Website 1");
+            websiteBase.Use(new User("User BB"));
+
+            websiteBase = websiteFactory.GetWebsite("Website 1");
+            websiteBase.Use(new User("User CC"));
+
+            websiteBase = websiteFactory.GetWebsite("Website 2");
+            websiteBase.Use(new User("User DD"));
+
+            websiteBase = websiteFactory.GetWebsite("Website 2");
+            websiteBase.Use(new User("User EE"));
+
+            Debug.Log("網站實體: " + websiteFactory.GetCount());
+        }
+
+    }
+}
